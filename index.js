cec40e61ec79d6d2bf013f32502ddbfce4a99253
Huy Pham <benjaminhuypham@gmail.com>
2019-10-07 20:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-function addingEventListener() {
-}
+const input = document.getElementById('input');
+input.addingEventListener('click', function addingEventListener() {
+  alert ('I was click'); 
+});
